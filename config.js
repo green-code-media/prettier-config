e63d63a37fdd692efa3ccb5024ae8a2a44904059
Ben Green <ben.green@greencodemedia.co.uk>
2023-11-12 11:43:28 +0000

    CHORE: initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: "es5",
+    arrowParens: "always",
+    quoteProps: "consistent",
+    bracketSpacing: false,
+    singleQuote: false,
+    useTabs: false,
+    semi: true,
+    tabWidth: 2,
+    printWidth: 120,
+}
